1595bc249b8424296beb6ce46173b9baa19a33e0
Navaneet R Rao <navaneet517@gmail.com>
2024-06-19 08:17:59 +0530

    feat: api funcs

---
+++ internal/middleware/authorization.go
@@ -0,0 +1,10 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/navaneet-rao/Go-API/api"
+	"github.com/navaneet-rao/Go-API/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
